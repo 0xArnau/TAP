49c059ee4f935734f543bd35c461edca1e9c7fe0
arnau <none>
2020-11-19 19:12:24 +0100

    empezado MailBox

--- mail-service/src/mailbox/MailBox.java
+++ mail-service/src/mailbox/MailBox.java
@@ -1,5 +1,27 @@
 package mailbox;
 
+import users.*;
+import messages.*;
+import mailstore.*;
+import system.*;
+
+import java.util.LinkedList;
+import java.util.List;
+
 public class MailBox {
-	
+	private User user;
+	private List<Message> messages;
+
+	public MailBox(User u) {
+		user = u;
+	}
+
+	public void updateMail() {;}
+	public void listMail() {;}
+	public void getSortedMail() {;}
+	public void filterMail() {;}
+
+	private List<Message> sortByTime() {return null;}
+	private List<Message> sortBySent() {return null;}
+	private List<Message> sortByIDK() {return null;}
 }
