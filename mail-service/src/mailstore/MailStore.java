b8a64248837139362d1e39ecdabb92598739b03e
arnau <none>
2020-12-14 12:08:39 +0100

    Interface Mailstore

--- mail-service/src/mailstore/MailStore.java
+++ mail-service/src/mailstore/MailStore.java
@@ -1,10 +1,15 @@
 package mailstore;
 
+import users.*;
+import messages.*;
+import java.util.List;
 /**
  * MailStore
  */
 public interface MailStore {
 
-	
+	final String regex = ";";
+	public static void sendMail(User u, Message m) {};
+	public static List<Message> getMail(User u)  {return null;};
 }
 
