1a99c61ba528df557546f647774637c4b567dc2b
Sergi Alier <sergi.alier@estudiants.urv.cat>
2020-12-18 19:07:28 +0100

    TestSystem.java: Modificado, faltan arreglar y añadir cosas

--- mail-service/src/TestSystem.java
+++ mail-service/src/TestSystem.java
@@ -13,16 +13,19 @@ public class TestSystem {
         //Test paso 1.1
         //Test paso 1.2
         System.out.println("--IN-MEMORY IMPLEMENTATION--");
+        System.out.println("Creando cuentas...");
 	    User user = new User("star", "Arnau", 2000);
 		User user1 = new User("Mr.Star", "Arnau", 2000);
 		User user2 = new User("John Wick", "Keanu Reeves", 1964);
 		User user3 = new User("betaSAV", "Sergi", 1999);
-		User user4 = new User("Mr.Jhonny", "Josep", 1985);
+        User user4 = new User("Mr.Jhonny", "Josep", 1985);
+        
 		MailStore star = new InMemory();
 		MailStore mrStar = new InMemory();
 		MailStore john = new InMemory();
 		MailStore beta = new InMemory();
-		MailStore jhonny = new InMemory();
+        MailStore jhonny = new InMemory();
+        
 		MailBox starBox = MailSystem.newUser(user, star);
 		MailBox mrStarBox = MailSystem.newUser(user1, mrStar);
 		MailBox johnBox = MailSystem.newUser(user2, john);
@@ -30,6 +33,7 @@ public class TestSystem {
         MailBox jhonnyBox = MailSystem.newUser(user4, jhonny);
         
         //Test paso 1.3
+        System.out.println("Enviando correos...");
         starBox.sendMail(user1.getUserName(), "subject", "body");
         mrStarBox.sendMail(user.getUserName(), "subject", "body");
         betaBox.sendMail(user2.getUserName(), "Nota", "Hola profe, quina nota tindré al final del treball?");
@@ -37,6 +41,7 @@ public class TestSystem {
 
         //Test paso 1.4
         //Test paso 1.5
+        System.out.println("Actualizando correo star...");
         starBox.updateMail().forEach(System.out::println);
 
         //Test paso 1.6 A HACER
@@ -44,6 +49,7 @@ public class TestSystem {
 
         //Test paso 1.7
         //Test paso 1.8
+        System.out.println("Correos con el sujeto \"Subject\"");
         starBox.filterSubject("subject").forEach(System.out::println);
 
         //Test paso 1.9 A HACER
@@ -53,23 +59,25 @@ public class TestSystem {
         System.out.println("Mensajes en el sistema: " + MailSystem.countMessages());
 
         //Test paso 1.11
+        System.out.println("Mensajes de media por usuario: ");
         MailSystem.averageMessagesPerUser();
 
         //Test paso 1.12 A HACER
 
         //Test paso 1.13
         //Test paso 1.14
-        System.out.println("Palabras totales enviadas por Arnau " + MailSystem.countWordsOfMessagesFromUser("Arnau"));
+        System.out.println("Palabras totales enviadas por Arnau: " + MailSystem.countWordsOfMessagesFromUser("Arnau"));
 
         //Test paso 1.15
+        System.out.println("Correos enviados por usuarios nacidos antes del 2000: ");
         MailSystem.usersBornBeforeXYear(2000).forEach(System.out::println);
-        System.out.println("--FILE IMPLEMENTATION--");
+
+        MailSystem.reset();
 
         //Test paso 1.16
         //Test paso 2.2
-
-        MailSystem.reset();
-        
+        System.out.println("--FILE IMPLEMENTATION--");
+        System.out.println("Creando cuentas...");
         user = new User("star", "Arnau", 2000);
 		user1 = new User("Mr.Star", "Arnau", 2000);
 		user2 = new User("John Wick", "Keanu Reeves", 1964);
@@ -87,7 +95,9 @@ public class TestSystem {
 		johnBox = MailSystem.newUser(user2, john);
 		betaBox = MailSystem.newUser(user3, beta);
         jhonnyBox = MailSystem.newUser(user4, jhonny);
+
         //Test paso 2.3
+        System.out.println("Enviando correos...");
         starBox.sendMail(user1.getUserName(), "subject", "body");
         mrStarBox.sendMail(user.getUserName(), "subject", "body");
         betaBox.sendMail(user2.getUserName(), "Nota", "Hola profe, quina nota tindré al final del treball?");
@@ -95,6 +105,7 @@ public class TestSystem {
 
         //Test paso 2.4
         //Test paso 2.5
+        System.out.println("Actualizando correo star...");
         starBox.updateMail().forEach(System.out::println);
 
         //Test paso 2.6 A HACER
@@ -102,24 +113,26 @@ public class TestSystem {
 
         //Test paso 2.7
         //Test paso 2.8
-        starBox.filterSubject("Nota").forEach(System.out::println);
+        System.out.println("Correos con el sujeto \"Subject\"");
 
         //Test paso 2.9 A HACER
         //MailSystem.filterSubject("").forEach(System.out::println);
 
         //Test paso 2.10
-        System.out.println(MailSystem.countMessages());
+        System.out.println("Mensajes en el sistema: " + MailSystem.countMessages());
 
         //Test paso 2.11
+        System.out.println("Mensajes de media por usuario: ");
         MailSystem.averageMessagesPerUser();
 
         //Test paso 2.12 A HACER
 
         //Test paso 2.13
         //Test paso 2.14
-        System.out.println(MailSystem.countWordsOfMessagesFromUser("Arnau"));
+        System.out.println("Palabras totales enviadas por Arnau: " + MailSystem.countWordsOfMessagesFromUser("Arnau"));
 
         //Test paso 2.15
+        System.out.println("Correos enviados por usuarios nacidos antes del 2000: ");
         MailSystem.usersBornBeforeXYear(2000).forEach(System.out::println);
 
     }
