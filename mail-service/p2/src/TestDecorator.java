dd8f353e2f98fc97b1dcec45904f2af678cd117c
Sergi Alier <sergi.alier@estudiants.urv.cat>
2020-12-26 16:59:42 +0100

    TestDecorator.java: Hecho

--- mail-service/p2/src/TestDecorator.java
+++ mail-service/p2/src/TestDecorator.java
@@ -1,5 +1,54 @@
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
+import mailstore.EncodeDecorator;
+import mailstore.MailStore;
+import mailstore.OnFile;
+import messages.Message;
+
+@SuppressWarnings("unused")
 public class TestDecorator {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchPaddingException {
+		String key = "IWantToPassTAP12"; // 128 bit key
+		java.security.Key aesKey = new javax.crypto.spec.SecretKeySpec(key.getBytes(), "AES");
+		Cipher cipher = Cipher.getInstance("AES");
+
+		System.out.println("Key: " + key);
+		System.out.println(aesKey);
+		System.out.println(cipher);
+
+		System.out.println("Encryption in process...");
+		byte[] encrypted = new byte[0];
+		try {
+			cipher.init(Cipher.ENCRYPT_MODE, aesKey);
+			encrypted = cipher.doFinal(key.getBytes());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		String encryption = Base64.getEncoder().encodeToString(encrypted);
+		System.out.println(encryption);
 		
+		System.out.println("Decryption in process...");
+		byte[] encrypted1 = Base64.getDecoder().decode(encryption.getBytes());
+		String decrypted = null;
+		try {
+			cipher.init(Cipher.DECRYPT_MODE, aesKey);
+			decrypted = new String(cipher.doFinal(encrypted1));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("Decorator");
+		MailStore none = new OnFile();
+		EncodeDecorator deco = new EncodeDecorator(none);
+		deco.setCipher();
+		deco.sendMail("star", new Message("star", "star", "subject", "body"));
+		System.out.println("Mail decrypted: ");
+		try {
+			deco.getMail("star").forEach(System.out::println);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
