1d0ca646702483a3f6a07929d59297e3767b6a38
Sergi Alier <sergi.alier@estudiants.urv.cat>
2020-12-27 17:11:05 +0100

    P2: Javadoc hecho.

--- mail-service/p2/src/mailbox/AutomaticMailBox.java
+++ mail-service/p2/src/mailbox/AutomaticMailBox.java
@@ -10,30 +10,59 @@ import java.util.stream.Stream;
 import mailstore.*;
 import messages.Message;
 
-public class AutomaticMailBox extends MailBox implements Subject{
+public class AutomaticMailBox extends MailBox implements Subject {
 	private List<Observer> list = new LinkedList<Observer>();
-	
-	public AutomaticMailBox (String u, MailStore store) {
+
+	/**
+	 * Constructor de AutomaticMailBox.
+	 * 
+	 * @param u     Nombre de usuario.
+	 * @param store MailStore del usuario.
+	 */
+	public AutomaticMailBox(String u, MailStore store) {
 		super(u, store);
 	}
 
-	//<Observer>
+	// <Observer>
+	/**
+	 * Acción que permite añadir un Observer.
+	 * 
+	 * @param o Observer que se va a añadir.
+	 */
 	@Override
 	public void attach(Observer o) {
 		list.add(o);
 	}
+
+	// <Observer>
+	/**
+	 * Acción que permite borrar un Observer de la lista.
+	 * 
+	 * @param o Observer que se va a borrar de la lista.
+	 */
 	@Override
 	public void detach(Observer o) {
 		list.remove(o);
 	}
+
+	/**
+	 * Acción que notificará a todos los Observers de la actualización.
+	 */
 	@Override
 	public void notifyObservers() {
-		for (Observer o: list) {
+		for (Observer o : list) {
 			o.update(super.listMail());
 		}
 	}
 
-	//</Observer>
+	// </Observer>
+	/**
+	 * Función encargada de actualizar el correo recibido.
+	 * 
+	 * @return Devuelve on stream de todos los correos recibidos.
+	 * @throws Exception Caso edge donde se pida actualizar el correo de alguien
+	 *                   inexistente.
+	 */
 	@Override
 	public Stream<Message> updateMail() throws Exception {
 		super.updateMail();
@@ -41,25 +70,30 @@ public class AutomaticMailBox extends MailBox implements Subject{
 		return super.listMail().stream();
 	}
 
+	/**
+	 * Función que permite obtener todos los usuarios que son considerados spammers.
+	 * 
+	 * @return Devuelve una colección con el nombre de los usuarios considerados
+	 *         spammers.
+	 */
 	public Set<String> getSpammers() {
-		Set<String>  spammers = new HashSet<String>();
-		for (Observer o: list) {
+		Set<String> spammers = new HashSet<String>();
+		for (Observer o : list) {
 			try {
 				SpamUserFilter suf = ((SpamUserFilter) o);
-				for (String from :suf.getMessages().stream().map(p -> p.getFrom()).collect(Collectors.toSet())) {
+				for (String from : suf.getMessages().stream().map(p -> p.getFrom()).collect(Collectors.toSet())) {
 					spammers.add(from);
 				}
 			} catch (Exception e) {
 			}
 			try {
 				TooLongFilter stf = ((TooLongFilter) o);
-				for (String from :stf.getMessages().stream().map(p -> p.getFrom()).collect(Collectors.toSet())) {
+				for (String from : stf.getMessages().stream().map(p -> p.getFrom()).collect(Collectors.toSet())) {
 					spammers.add(from);
 				}
 			} catch (Exception e) {
-			}	
+			}
 		}
-		
 		return spammers;
 	}
 }
--- mail-service/p2/src/mailstore/EncodeDecorator.java
+++ mail-service/p2/src/mailstore/EncodeDecorator.java
@@ -14,23 +14,47 @@ public class EncodeDecorator extends MessageDecorator {
 	private Cipher cipher = null;
 	private java.security.Key aesKey = null;
 
+	/**
+	 * Constructor de EncodeDecorator
+	 * 
+	 * @param store MailStore de la que se heredan sus atributos.
+	 */
 	public EncodeDecorator(MailStore store) {
 		super(store);
 	}
 
+	/**
+	 * Función que hace override a getMail de MessageDecorator. Permite obtener los
+	 * correos del buzón del usuario u.
+	 * 
+	 * @param u Usuario del que se quiere conseguir los correos.
+	 * @return Devuelve una lista de los mensajes del usuario.
+	 */
 	@Override
 	public List<Message> getMail(String u) throws Exception {
 		return super.getMail(u).stream()
-			.map(p -> new Message(p.getFrom(), p.getTo(), p.getSubject(), decrypt(p.getBody())))
-			.collect(Collectors.toList());
+				.map(p -> new Message(p.getFrom(), p.getTo(), p.getSubject(), decrypt(p.getBody())))
+				.collect(Collectors.toList());
 	}
 
+	/**
+	 * Acción que hace override a sendMail de MessageDecorator. Encargada de enviar
+	 * un correo de un usuario a otro.
+	 * 
+	 * @param u Persona a la que irá a parar el correo.
+	 * @param m Variable de tipo Message (Consultar Message.java para más
+	 *          información).
+	 */
 	@Override
 	public void sendMail(String u, Message m) {
 		Message message = new Message(m.getFrom(), m.getTo(), m.getSubject(), encrypt(m.getBody()));
 		super.sendMail(u, message);
 	}
 
+	/**
+	 * Acción encargada de iniciar el Cipher con llave IWantToPassTAP12 decidida por
+	 * enunciado.
+	 */
 	public void setCipher() {
 		String key = "IWantToPassTAP12"; // 128 bit key
 		aesKey = new javax.crypto.spec.SecretKeySpec(key.getBytes(), "AES");
@@ -43,23 +67,38 @@ public class EncodeDecorator extends MessageDecorator {
 		}
 	}
 
+	/**
+	 * Función encargada de encriptar el mensaje de un usuario.
+	 * 
+	 * @param body Contenido del mensaje a encriptar.
+	 * @return Devuelve el mensaje encriptado de tipo String.
+	 */
 	public String encrypt(String body) {
 		if (cipher == null || aesKey == null)
 			return null;
-			byte[] encrypted = new byte[0];
-			try {cipher.init(Cipher.ENCRYPT_MODE, aesKey);
-				encrypted = cipher.doFinal(body.getBytes());
-			} catch (Exception e) {e.printStackTrace();
-			}
-			return  Base64.getEncoder().encodeToString(encrypted);
+		byte[] encrypted = new byte[0];
+		try {
+			cipher.init(Cipher.ENCRYPT_MODE, aesKey);
+			encrypted = cipher.doFinal(body.getBytes());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return Base64.getEncoder().encodeToString(encrypted);
 	}
 
+	/**
+	 * Función encargada de deshacer la encriptación.
+	 * 
+	 * @param body Contenido del mensaje a desencriptar.
+	 * @return Devuelve el mensaje desencriptado de tipo String.
+	 */
 	public String decrypt(String body) {
 		if (cipher == null || aesKey == null)
 			return null;
 		byte[] encrypted1 = Base64.getDecoder().decode(body.getBytes());
 		String decrypted = null;
-		try {cipher.init(Cipher.DECRYPT_MODE, aesKey);
+		try {
+			cipher.init(Cipher.DECRYPT_MODE, aesKey);
 			decrypted = new String(cipher.doFinal(encrypted1));
 		} catch (Exception e) {
 			e.printStackTrace();
