5a575fe7cec81ee78527cb13bda23bbc17e1b3ac
arnau <none>
2020-12-27 18:30:56 +0100

    P3 -> Factory => No se si es así

--- mail-service/p3/src/TestP3.java
+++ mail-service/p3/src/TestP3.java
@@ -1,12 +1,15 @@
+import mailstore.EncodeDecorator;
 import mailstore.MailStore;
 import mailstore.OnFile;
 import mailstore.RedisMailStore;
 import mailstore.RedisClient;
 import mailstore.StoreAdapter;
 import messages.Message;
+import msfactory.MailStoreFactory;
+import msfactory.StoreType;
 
 public class TestP3 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
 		RedisMailStore rsm = RedisClient.getInstance();
 		RedisMailStore rsm1 = RedisClient.getInstance();
@@ -31,5 +34,19 @@ public class TestP3 {
 		}
 		rsm.flushAll();
 		rsm1.flushAll();
+
+		MailStore file = MailStoreFactory.createMailStore(StoreType.FILE);
+		((EncodeDecorator) file).setCipher();
+		System.out.println("File");
+		file.sendMail("star",new Message("star","star","klk","1234567878990"));
+		file.getMail("star").forEach(System.out::println);
+		MailStore redis = MailStoreFactory.createMailStore(StoreType.REDIS);
+		System.out.println("Redis");
+		redis.sendMail("star",new Message("star","star","qwe","123213213"));
+		redis.getMail("star").forEach(System.out::println);
+		MailStore memo = MailStoreFactory.createMailStore(StoreType.MEMORY);
+		System.out.println("Mem");
+		memo.sendMail("star",new Message("star","star", "subject"," body"));
+		memo.getMail("star").forEach(System.out::println);
 	}
 }
